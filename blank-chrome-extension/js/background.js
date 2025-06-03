05ec3a74d719a70db7c9aad40fa37021d1aef234
Oliver Dunk <oliverdunk@google.com>
2025-06-03 18:07:14 +0100

    Add additional sample code

--- blank-chrome-extension/js/background.js
+++ blank-chrome-extension/js/background.js
@@ -1,2 +1,9 @@
-// Add JS here
-console.log('Hello background!');
+// Add code for your extension service worker here. This code runs in the
+// background and should listen to relevant events for your extension.
+// Learn more: https://developer.chrome.com/docs/extensions/develop/concepts/service-workers
+
+chrome.runtime.onInstalled.addListener(details => {
+  if (details.reason === chrome.runtime.OnInstalledReason.INSTALL) {
+    console.log('Hello background!');
+  }
+});
--- blank-chrome-extension/js/content.js
+++ blank-chrome-extension/js/content.js
@@ -1,2 +1,5 @@
-// Add JS here
+// Add code for your content script here. Any code added to your content script
+// will be injected and run on https://example.com.
+// Learn more: https://developer.chrome.com/docs/extensions/develop/concepts/content-scripts
+
 console.log('Hello content script!');
