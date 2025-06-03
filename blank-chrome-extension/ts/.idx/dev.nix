0b2bc343e22d8dd5208f49b27d4c672f8379f85b
Oliver Dunk <oliverdunk@google.com>
2025-06-03 17:56:33 +0100

    Fix default open files

--- blank-chrome-extension/ts/.idx/dev.nix
+++ blank-chrome-extension/ts/.idx/dev.nix
@@ -21,7 +21,7 @@
         # Example: install JS dependencies from NPM
         # npm-install = "npm install";
         # Open editors for the following files by default, if they exist:
-        default.openFiles = [ "manifest.json" "background.js" "content.js" ];
+        default.openFiles = [ "static/manifest.json" "src/background.ts" "src/content.ts" ];
       };
       # Runs when the workspace is (re)started
       onStart = {
