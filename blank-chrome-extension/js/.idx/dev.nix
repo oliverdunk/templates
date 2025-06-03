618d205ed9eee23f0057f53c46ba038077c5ef12
Oliver Dunk <oliverdunk@google.com>
2025-06-03 19:01:09 +0100

    Always open README by default

--- blank-chrome-extension/js/.idx/dev.nix
+++ blank-chrome-extension/js/.idx/dev.nix
@@ -22,7 +22,7 @@
         # Example: install JS dependencies from NPM
         # npm-install = "npm install";
         # Open editors for the following files by default, if they exist:
-        default.openFiles = [ "background.js" ];
+        default.openFiles = [ "README.md" ];
       };
       # Runs when the workspace is (re)started
       onStart = {
